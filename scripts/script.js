d5fbee8d63485d0068833c327cfa70244dcfca9d
K1111n <kaja_nan@hotmail.de>
2025-07-11 19:20:06 +0200

    funkts

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,13 @@
 function renderBook() {
-  let bookRef = document.getElementById("book-section");
-  bookRef.innerHTML = "";
   for (let i = 0; i < books.length; i++) {
-    for (let j = 0; j < books.comments.length; j++) {
-      bookRef.innerHTML += bookTemplate(i, j);
+    let bookRef = document.getElementById("book-section");
+    bookRef.innerHTML += bookTemplate(i);
+  }
+  let commentSection = document.getElementsByClassName("comment-section");
+  for (let i = 0; i < books.length; i++) {
+    let commentRef = commentSection[i];
+    for (let j = 0; j < books[i].comments.length; j++) {
+      commentRef.innerHTML += commentTemplate(i, j);
     }
   }
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,17 +1,28 @@
-function bookTemplate(i, j) {
+function bookTemplate(i) {
   return `
-<div class="book-section">
-    <h2>${books[i].name}</h2>
-    <img>
-    <p>${books[i].price} </p><br><p>${books[i].likes} Likes</p><img class="liked"><img class="not_liked"><br>
+  <div class="book-section">
+    <div>
+        <h2>${books[i].name}</h2>
+        <div class="book-img"><img></div>
+        <div class="price-likes"><p>${books[i].price} </p><br><p>${books[i].likes} Likes</p><img class="liked"><img class="not-liked"><br></div>
         <div class="table">
-         <p>Author: ${books[i].author}</p> <br>
-         <p>Erscheinungsjahr: ${books[i].publishedYear}</p> <br>
-         <p>Genre: ${books[i].genre}</p> <br>
-        </div>
-    <br>
+            <p>Author: ${books[i].author}</p> <br>
+            <p>Erscheinungsjahr: ${books[i].publishedYear}</p> <br>
+            <p>Genre: ${books[i].genre}</p> <br>
+        </div>    
             <h3>Kommentare</h3><br>
-    <p>${books[i].comments[j].name}:</p><p>${books[i].comments[j].comment}</p>
+    </div>
+    <div class="comment-section">
+    </div>
+    <br>
+    
+    <div class="input-line"><input type="text" placeholder="Schreibe dein Kommentar"></input><img src="paper-plane.png" alt="absenden" onclick="submit()"></div>
 </div>
    `;
+}
+
+function commentTemplate(i, j) {
+  return `
+    <p>${books[i].comments[j].name}:</p><p>${books[i].comments[j].comment}</p><br>
+    `;
 }
